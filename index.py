de87393f7f881afeb53f53470bf61c0cd95f9e08
OtherJack <jacksweeney@outlook.com>
2017-11-16 18:44:25 +0000

    index completed

--- index.py
+++ index.py
@@ -5,3 +5,31 @@ app = Flask(__name__)
 @app.route('/')
 def root():
   return render_template('index.html'), 200
+
+@app.route('/config/')
+def config():
+  str = []
+  str.append('Debug:'+app.config['DEBUG'])
+  str.append('port:'+app.config['port'])
+  str.append('url:'+app.config['url'])
+  str.append('ip_address:'+app.config['ip_address'])
+  return '/t'.join(str)
+
+def init(app):
+  config = ConfigParser.ConfigParser()
+  try:
+      config_location = "etc/defaults.cfg"
+      config.read(config_location)
+
+      app.config['DEBUG'] = config.get("config", "debug")
+      app.config['ip_address'] = config.get("config", "ip_address")
+      app.config['port'] = config.get("config","port")
+      app.config['url'] = config.get("config", "url")
+  except:
+      print "Could not read configs from: ", config_location
+
+if __name__ == '__main__':
+  init(app)
+  app.run(
+      host=app.config['ip_address'],
+      port=int(app.config['port']))
