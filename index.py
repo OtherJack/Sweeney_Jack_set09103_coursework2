7d6abd3603842af04b89c7adaf12c3d5d1333e5e
OtherJack <jacksweeney@outlook.com>
2017-11-20 19:10:57 +0000

    rough database added

--- index.py
+++ index.py
@@ -1,9 +1,53 @@
 import ConfigParser
 import logging
+import sqlite3
 from logging.handlers import RotatingFileHandler
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, g
 
 app = Flask(__name__)
+db_location = 'var/films.db'
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = sqlite3.connect(db_location)
+        g.db = db
+    return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+          db = get_db()
+          with app.open_resource('var/schema.sql', mode='r') as f:
+              db.cursor().executescript(f.read())
+          db.commit()
+
+@app.route("/FeaturedFilms")
+def films():
+    db = get_db()
+
+    page = []
+    page.append('<html><ul>')
+    sql = "SELECT rowid, * FROM films ORDER BY ID"
+    for row in db.cursor().execute(sql):
+        page.append('<li>')
+        page.append(str(row))
+        page.append('</li>')
+
+    page.append('</ul></html>')
+    return ''.join(page)
+    
+    #g.db = films.db
+    #cur = g. db.execute('SELECT * FROM films')
+    #Films = [dict(ID=row[0], title=row[1], director=row[2], stars=row[3],release_date=row[4], rating=row[5]) for row in cur.fetchall()]
+    #g.db.close()
+    #return render_template('featured.html', films=films), 200
+
 
 @app.route('/')
 def root():
---
+++ var/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE if EXISTS films;
+
+CREATE TABLE films (
+    ID text,
+    title text,
+    director text,
+    stars text,
+    release_date text,
+    rating text
+);
